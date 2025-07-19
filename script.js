d73c2676cc61f79e3cc2a9f820f73ee96daa128c
Murtaza <murtaza.mod@gmail.com>
2025-07-19 17:23:55 +0300

    Change the condition of the new grid creation
    to make sure the input is valid (1-100)

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ createNewGridButton.addEventListener("click",()=>{
     let num = prompt("how many cells do you want on each side?");
     //A larger number of squares results in more computer resources being used,
     //potentially causing delays, freezing, or crashing that we want to prevent.
-    if(num>=100){
-        alert("number is too big make sure it's less than 100");
-    }else{
+    if(1<= num <= 100){
         containerDiv.textContent = "";
         createGrid(num);
+    }else{
+        alert("Invalid input make sure it's from 1 - 100")
     }
 })
 function createGrid(numPerSide = 16){
