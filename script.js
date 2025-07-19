f19f5185d26911e324521953ba7eb82e1a6cbff0
Murtaza <murtaza.mod@gmail.com>
2025-07-19 17:02:12 +0300

    Set a limit to new grid dimentions
    by creating a condition if it's more or equal to 100 then showing a
    message telling the user that the number is too big and goes back to
    the original grid because to prevent delays, freezing, or crashing

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let containerDiv = document.querySelector(".grid");
 createNewGridButton = document.querySelector("button")
 createNewGridButton.textContent = "New Grid";
 createNewGridButton.addEventListener("click",()=>{
-    containerDiv.textContent = "";
     let num = prompt("how many cells do you want on each side?");
-    createGrid(num);
+    if(num>=100){
+        alert("number is too big make sure it's less than 100");
+    }else{
+        containerDiv.textContent = "";
+        createGrid(num);
+    }
 })
 function createGrid(numPerSide = 16){
     for(let i = 0; i<numPerSide; i++){
