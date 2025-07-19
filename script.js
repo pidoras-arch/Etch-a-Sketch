ec42d24e4de1297a63c5219d294d9770aa091897
Murtaza <murtaza.mod@gmail.com>
2025-07-19 16:45:06 +0300

    Add a button and a function to create a new grid
    we converted the grid creation into a function so that we can call it
    later to create a new one and added a button in the html file, this
    button clears the grid container then asks the user for the dimentions
    of the new grid and calls the createGrid function

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const containerDiv = document.querySelector(".container");
-for(let i = 0; i<16; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for(let j = 0; j<16; j++){
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        row.appendChild(cell);
+let containerDiv = document.querySelector(".grid");
+createNewGridButton = document.querySelector("button")
+createNewGridButton.textContent = "New Grid";
+createNewGridButton.addEventListener("click",()=>{
+    containerDiv.textContent = "";
+    let num = prompt("how many cells do you want on each side?");
+    createGrid(num);
+})
+function createGrid(numPerSide = 16){
+    for(let i = 0; i<numPerSide; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j<numPerSide; j++){
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+            cell.addEventListener("mouseover",()=> cell.style.backgroundColor = "green")
+        }
+        containerDiv.appendChild(row);
     }
-    containerDiv.appendChild(row);
 }
+createGrid();
